0b5850e18955e8bf4920ca07e1557be0813b31c7
Đức Quang <phanducquang07@gmail.com>
2018-12-09 16:49:41 +0700

    giao diện tạo nhóm chi tiêu, hiển thị nhóm chi tiêu

---
+++ app/src/main/java/vn/edu/uit/quanlychitieunhom/Views/HienThiNhomChiTieu.java
@@ -0,0 +1,20 @@
+package vn.edu.uit.quanlychitieunhom.Views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import vn.edu.uit.quanlychitieunhom.R;
+
+public class HienThiNhomChiTieu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hien_thi_nhom_chi_tieu);
+        addControl();
+    }
+
+    private void addControl() {
+    }
+}
