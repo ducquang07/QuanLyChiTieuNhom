6f6219ec6840e6be1ee0ab4096af36443ea491c8
Đức Quang <phanducquang07@gmail.com>
2018-12-22 11:37:41 +0700

    upload ảnh

--- app/src/main/java/vn/edu/uit/quanlychitieunhom/Views/ThemGiaoDich.java
+++ app/src/main/java/vn/edu/uit/quanlychitieunhom/Views/ThemGiaoDich.java
@@ -3,11 +3,15 @@ package vn.edu.uit.quanlychitieunhom.Views;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -21,11 +25,22 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 
 import org.json.JSONException;
@@ -37,6 +52,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -62,7 +78,16 @@ public class ThemGiaoDich extends AppCompatActivity {
     int ERR = 0;
     Util util = new Util();
     int StatusCode;
+    private ImageButton btnChooseImage;
+    private ImageButton btnUploadImage;
+    private ImageView imageView;
 
+    private Uri filePath;
+
+    private final int PICK_IMAGE_REQUEST = 71;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     private ProgressBar progressBar;
     private Spinner spLoaiChiTieu;
@@ -73,6 +98,7 @@ public class ThemGiaoDich extends AppCompatActivity {
     private int mYear;
     private int mMonth;
     private int mDay;
+    private Uri downloadUri;
 
     private kychitieu kychitieu;
     private List<loaigiaodich> List_LoaiGiaoDich = new ArrayList<>();
@@ -86,6 +112,8 @@ public class ThemGiaoDich extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_giao_dich);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         GetComponentByID();
         addControls();
         GetNhomChiTieu();
@@ -132,6 +160,20 @@ public class ThemGiaoDich extends AppCompatActivity {
             }
         });
 
+        btnChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        btnUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
         txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -151,6 +193,81 @@ public class ThemGiaoDich extends AppCompatActivity {
         updateDisplay();
     }
 
+    private void uploadImage() {
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            final StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString()+".jpg");
+            UploadTask uploadTask = ref.putFile(filePath);
+            Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    // Continue with the task to get the download URL
+                    return ref.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        downloadUri = task.getResult();
+                        progressDialog.dismiss();
+                        Toast.makeText(ThemGiaoDich.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Handle failures
+                        // ...
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(ThemGiaoDich.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                            .getTotalByteCount());
+                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                }
+            });
+        }
+    }
+
+    //upload Image
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //end upload Image
 
     public void GetComponentByID(){
         txtDate = (EditText) findViewById(R.id.txtDate);
@@ -159,6 +276,9 @@ public class ThemGiaoDich extends AppCompatActivity {
         txtSotien = findViewById(R.id.txtSotien);
         txtGhichu = findViewById(R.id.txtGhichu);
         progressBar = (ProgressBar) findViewById(R.id.proBarThemGiaoDich);
+        btnChooseImage = findViewById(R.id.btnChooseImage);
+        btnUploadImage = findViewById(R.id.btnUploadImage);
+        imageView = findViewById(R.id.viewImage);
     }
 
     //TODO: The callback received when the user "sets" the date in the Dialog
@@ -200,7 +320,7 @@ public class ThemGiaoDich extends AppCompatActivity {
         new_giaodich = new giaodich(util.StringToDate(txtDate.getText().toString(),"dd/MM/yyyy"),
                                     Double.parseDouble(txtSotien.getText().toString()),
                                     txtGhichu.getText().toString(),
-                                    null,
+                                    downloadUri.toString(),
                                     NhomChiTieu,
                                     create_by,
                                     LoaiGiaoDich,
